a20072051e1c6088e1a247139e289da38a049078
Mateusz Front <mateusz.front@swmansion.com>
2023-11-08 15:59:42 +0100

    update to core 1.0 API

--- test/membrane_realtimer_test.exs
+++ test/membrane_realtimer_test.exs
@@ -13,20 +13,20 @@ defmodule Membrane.RealtimerTest do
       %Buffer{pts: Time.milliseconds(100), payload: 1}
     ]
 
-    structure = [
+    spec = [
       child(:src, %Testing.Source{output: Testing.Source.output_from_buffers(buffers)})
       |> child(:realtimer, Realtimer)
       |> child(:sink, Testing.Sink)
     ]
 
-    pipeline = Testing.Pipeline.start_link_supervised!(structure: structure)
+    pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
 
     assert_sink_buffer(pipeline, :sink, %Buffer{payload: 0})
     refute_sink_buffer(pipeline, :sink, _buffer, 90)
     assert_sink_buffer(pipeline, :sink, %Buffer{payload: 1}, 20)
     assert_end_of_stream(pipeline, :sink)
     refute_sink_buffer(pipeline, :sink, _buffer, 0)
-    Testing.Pipeline.terminate(pipeline, blocking?: true)
+    Testing.Pipeline.terminate(pipeline)
   end
 
   test "Start following the time of the first buffer" do
@@ -36,15 +36,15 @@ defmodule Membrane.RealtimerTest do
       %Buffer{pts: Time.seconds(10), payload: 0}
     ]
 
-    structure = [
+    spec = [
       child(:src, %Testing.Source{output: Testing.Source.output_from_buffers(buffers)})
       |> child(:realtimer, Realtimer)
       |> child(:sink, Testing.Sink)
     ]
 
-    pipeline = Testing.Pipeline.start_link_supervised!(structure: structure)
+    pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
     assert_sink_buffer(pipeline, :sink, %Buffer{payload: 0}, 200)
     assert_end_of_stream(pipeline, :sink)
-    Testing.Pipeline.terminate(pipeline, blocking?: true)
+    Testing.Pipeline.terminate(pipeline)
   end
 end
